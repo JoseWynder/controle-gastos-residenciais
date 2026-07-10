dbedbb930c2a0427755f945d533db6ced0f47ce3
Jose Wynder <josewynderhernandes@gmail.com>
2026-07-09 23:24:34 -0300

    feat: add person service

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ControleGastos.Api.Data;
+using ControleGastos.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<PessoaService>();
 
 var app = builder.Build();
 
---
+++ backend/Services/PessoaService.cs
@@ -0,0 +1,45 @@
+﻿using ControleGastos.Api.Data;
+using ControleGastos.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.Api.Services;
+
+public class PessoaService
+{
+    private readonly AppDbContext _context;
+
+    public PessoaService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Pessoa>> ListarAsync()
+    {
+        return await _context.Pessoas
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<Pessoa> CriarAsync(Pessoa pessoa)
+    {
+        _context.Pessoas.Add(pessoa);
+        await _context.SaveChangesAsync();
+
+        return pessoa;
+    }
+
+    public async Task<bool> DeletarAsync(int id)
+    {
+        var pessoa = await _context.Pessoas.FindAsync(id);
+
+        if (pessoa is null)
+        {
+            return false;
+        }
+
+        _context.Pessoas.Remove(pessoa);
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+}
