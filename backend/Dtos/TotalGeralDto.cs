6cb9d2b242b7c20de09275657a9d8eaad7df5ad7
Jose Wynder <josewynderhernandes@gmail.com>
2026-07-10 14:36:27 -0300

    feat: add totals DTOs

---
+++ backend/Dtos/TotalGeralDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ControleGastos.Api.Dtos;
+
+public class TotalGeralDto
+{
+    public decimal TotalReceitas { get; set; }
+    public decimal TotalDespesas { get; set; }
+    public decimal Saldo { get; set; }
+}
