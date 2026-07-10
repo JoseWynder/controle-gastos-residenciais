e43531a69be0e98913505302a016b2a84582f681
Jose Wynder <josewynderhernandes@gmail.com>
2026-07-10 01:20:24 -0300

    feat: add transaction DTOs

---
+++ backend/Dtos/TransacaoDto.cs
@@ -0,0 +1,12 @@
+﻿using ControleGastos.Api.Entities;
+
+namespace ControleGastos.Api.Dtos;
+
+public class TransacaoDto
+{
+    public int Id { get; set; }
+    public string Descricao { get; set; } = string.Empty;
+    public decimal Valor { get; set; }
+    public TipoTransacao Tipo { get; set; }
+    public int PessoaId { get; set; }
+}
