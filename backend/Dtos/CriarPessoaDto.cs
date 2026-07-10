4dc93727bfc065ba7b3ff7441a5a46b99ed53562
Jose Wynder <josewynderhernandes@gmail.com>
2026-07-10 20:41:40 -0300

    feat: add portuguese validation messages

--- backend/Dtos/CriarPessoaDto.cs
+++ backend/Dtos/CriarPessoaDto.cs
@@ -1,12 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace ControleGastos.Api.Dtos;
 
 public class CriarPessoaDto
 {
-    [Required]
+    [Required(ErrorMessage = "O nome é obrigatório.")]
     public string Nome { get; set; } = string.Empty;
 
-    [Range(0, 130)]
+    [Range(0, 130, ErrorMessage = "A idade deve estar entre 0 e 130 anos.")]
     public int Idade { get; set; }
 }
